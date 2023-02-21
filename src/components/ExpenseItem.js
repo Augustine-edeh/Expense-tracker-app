9d19a87e33e7f99085d447eceb96d68dc55e6a31
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-02-21 20:57:51 +0100

    The Concept of Composition

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <h2>{props.title}</h2>
       <div className="expense-item__price">${props.amount}</div>
-    </div>
+    </Card>
   );
 }
 export default ExpenseItem;
